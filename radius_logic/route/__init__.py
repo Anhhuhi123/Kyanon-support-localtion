183701f90afea2b914f8987df8c86035c2be5cf2
benf-le <banglebang2016@gmail.com>
2026-01-15 15:09:06 +0700

    fix import path

--- radius_logic/route/__init__.py
+++ radius_logic/route/__init__.py
@@ -7,22 +7,21 @@ from .poi_validator import POIValidator
 from .score_calculator import ScoreCalculator
 
 # Import RouteBuilder from parent module (route.py)
-# We need to import from the parent package's route.py file
+# Use importlib to avoid circular import issues
 import importlib.util
 import os
 
-# Get path to route.py (sibling file in parent directory)
-_current_dir = os.path.dirname(os.path.abspath(__file__))
-_parent_dir = os.path.dirname(_current_dir)
-_route_py_path = os.path.join(_parent_dir, 'route.py')
+# Get the path to route.py (parent directory)
+parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+route_py_path = os.path.join(parent_dir, 'route.py')
 
 # Load route.py as a module
-_spec = importlib.util.spec_from_file_location("radius_logic_route_builder", _route_py_path)
-_route_module = importlib.util.module_from_spec(_spec)
-_spec.loader.exec_module(_route_module)
+spec = importlib.util.spec_from_file_location("radius_logic.route_module", route_py_path)
+route_module = importlib.util.module_from_spec(spec)
+spec.loader.exec_module(route_module)
 
 # Export RouteBuilder
-RouteBuilder = _route_module.RouteBuilder
+RouteBuilder = route_module.RouteBuilder
 
 __all__ = [
     'RouteConfig',
